7116e57351f8421966d01d42b253c919195a0b50
ThakurRsingh <raghavendrasingh1113@gmail.com>
2026-03-23 08:49:00 +0530

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -41,9 +41,9 @@ app.get("/api/test", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+ // app.get("*", (req, res) => {
+//    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+ // });
 }
 
 // Start server
